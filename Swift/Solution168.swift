516c2eb72c58cfc48e64c7e5708ef6b09a6ce32d
wanchun2012 <wanchun0616@gmail.com>
2016-11-22 23:25:58 +0000

    168. Excel Sheet Column Title

--- Swift/Solution168.swift
+++ Swift/Solution168.swift
@@ -7,9 +7,9 @@
     
     func convertToTitle(_ n: Int) -> String {
         return n == 0 ? "" :
-            self.convertToTitle(n/26) + String(Character(UnicodeScalar((n-1)%26 + Int(("A" as UnicodeScalar).value))!))
+            self.convertToTitle((n-1)/26) + String(Character(UnicodeScalar((n-1)%26 + Int(("A" as UnicodeScalar).value))!))
     }
  }
  
  let s = Solution168()
- print("convertToTitle: \(s.convertToTitle(27))")
+ print("convertToTitle: \(s.convertToTitle(26))")
